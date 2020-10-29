ac6c44e826ca21de72d570ed8fb221f005bc3dba
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-29 10:31:50 -0700

    updating JS code

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 //const moment = require('moment');
 
-let now = moment().format('MM DD YYYY');
+let now = moment().format("MM DD YYYY");
 console.log(now);
 //document.querySelector("#todayDate").textContent = "test";
 document.querySelector("#todayDate").textContent = now;
+
+let data = [
+  {
+    time: "09:00am",
+    schedule: "",
+  },
+  {
+    time: "10:00am",
+    schedule: "",
+  },
+  {
+    time: "11:00am",
+    schedule: "",
+  },
+  {
+    time: "12:00pm",
+    schedule: "",
+  },
+  {
+    time: "01:00pm",
+    schedule: "",
+  },
+  {
+    time: "02:00pm",
+    schedule: "",
+  },
+  {
+    time: "03:00pm",
+    schedule: "",
+  },
+  {
+    time: "04:00pm",
+    schedule: "",
+  },
+  {
+    time: "05:00pm",
+    schedule: "",
+  },
+];
