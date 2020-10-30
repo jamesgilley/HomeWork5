6ce5b7f712142693515c5b3732838f7575c61fd7
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-30 09:20:22 -0700

    update code

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
 //const moment = require('moment');
 
-let now = moment().format("MM DD YYYY");
+let now = moment().format('MM DD YYYY HH MM SS');
 console.log(now);
 //document.querySelector("#todayDate").textContent = "test";
 document.querySelector("#todayDate").textContent = now;
 
 let data = [
-  {
-    time: "09:00am",
-    schedule: "",
-  },
-  {
-    time: "10:00am",
-    schedule: "",
-  },
-  {
-    time: "11:00am",
-    schedule: "",
-  },
-  {
-    time: "12:00pm",
-    schedule: "",
-  },
-  {
-    time: "01:00pm",
-    schedule: "",
-  },
-  {
-    time: "02:00pm",
-    schedule: "",
-  },
-  {
-    time: "03:00pm",
-    schedule: "",
-  },
-  {
-    time: "04:00pm",
-    schedule: "",
-  },
-  {
-    time: "05:00pm",
-    schedule: "",
-  },
-];
+    {
+        time: '09:00am',
+        schedule: ''
+    },
+    {
+        time: '10:00am',
+        schedule: ''
+    },
+    {
+        time: '11:00am',
+        schedule: ''
+    },
+    {
+        time: '12:00pm',
+        schedule: ''
+    },
+    {
+        time: '01:00pm',
+        schedule: ''
+    },
+    {
+        time: '02:00pm',
+        schedule: ''
+    },
+    {
+        time: '03:00pm',
+        schedule: ''
+    },
+    {
+        time: '04:00pm',
+        schedule: ''
+    },
+    {
+        time: '05:00pm',
+        schedule: ''
+    }
+]
 
-$(".clear-button").on("click", function () {
-  localStorage.removeItem("data");
-  window.location.reload();
-});
+$('.clear-button').on('click', function(){
+    localStorage.removeItem('data')
+    window.location.reload()
+})
 
-function initializeApp() {
-  if (localStorage.getItem("data")) {
-    let store = JSON.parse(localStorage.getItem("data"));
-    data = store;
-  }
+function initializeApp(){
+    if(localStorage.getItem("data")){
+        let store = JSON.parse(localStorage.getItem('data'))
+        data = store
+    }
 }
 
 initializeApp();
 
 const saveInput = (i) => {
-  data[i].schedule = $(`.task_${i}`).val();
-  console.log(data);
-  localStorage.setItem("data", JSON.stringify(data));
-  alert("Data saved");
-};
+    data[i].schedule = $(`.task_${i}`).val()
+    console.log(data)
+    localStorage.setItem('data', JSON.stringify(data))
+    alert('Data saved')
+}
+
 
 let blocks = data.map((block, i) => {
-  return `<div class="schedule-block">
+    return (
+        `<div class="schedule-block">
         <div class="row">
             <div class="col-md-2 m-0 p-0">
                 <div class="time text-right">
@@ -76,15 +78,7 @@ let blocks = data.map((block, i) => {
             <div class="col-md-8 m-0 p-0">
                 <div class="task">
                 
-                    <input style="background: ${
-                      block.time.replace(":00", "") === moment().format("hha")
-                        ? "grey"
-                        : moment(block.time.replace(":00", ""), "hhA").isBefore(
-                            moment()
-                          )
-                        ? "red"
-                        : "green"
-                    };" class="task_${i}" value="${block.schedule}" />
+                    <input style="background: ${block.time.replace(':00', '') === moment().format('hha') ? 'grey' : moment(block.time.replace(':00', ''), 'hhA').isBefore(moment()) ? 'red' : 'green' };" class="task_${i}" value="${block.schedule}" />
                 </div>
             </div>
             <div class="col-md-2 m-0 p-0">
@@ -93,9 +87,11 @@ let blocks = data.map((block, i) => {
                 </div>
             </div>
         </div>
-    </div>`;
-});
+    </div>`
+    )
+})
 
 let html = blocks.join(" ")
 
 $('.schedule-container').html(html)
+
