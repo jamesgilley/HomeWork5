d129c991b1e819d5877191f5ccfe17a388b1862c
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-30 09:52:37 -0700

    updating javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //const moment = require('moment');
 
-let now = moment().format('MM DD YYYY HH MM SS');
+let now = moment().format('MM DD YYYY');
 console.log(now);
 //document.querySelector("#todayDate").textContent = "test";
 document.querySelector("#todayDate").textContent = now;
